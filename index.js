ffc8bdd77051d7344e287c38d2995848e54b24d6
thiagohenriquedev <thiagohenriqudev@gmail.com>
2022-06-01 18:55:27 -0300

    project finishid methods dom

---
+++ index.js
@@ -0,0 +1,84 @@
+const main = document.getElementById('main')
+const addUserBtn = document.getElementById('add-user')
+const doubleBtn = document.getElementById('double')
+const showMillionareBtn = document.getElementById('show-millionaires')
+const sortBtn = document.getElementById('sort')
+const calculateWealthBtn = document.getElementById('calculate')
+
+
+
+let data = []
+
+async function getRandomUser(){
+    const res = await fetch ('https://randomuser.me/api')
+    const data = await res.json();
+    const user = data.results[0]
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    }
+    addData(newUser);
+
+}
+
+// add new obj to data arr
+function addData(obj){
+    data.push(obj)
+    updateDOM()
+}
+
+// double money
+
+function doubleMoney(){
+    data = data.map((user) => {
+        return {...user, money: user.money * 2
+        }
+       
+    })
+    updateDOM()
+}
+
+// filter only milliionaires
+function showMillionaires(){
+    data = data.filter((user)=> 
+        user.money >  1000000)
+    updateDOM()
+}
+
+function sortByRichest(){
+    data.sort((a,b) => b.money - a.money)
+    updateDOM()
+}
+
+//calculate wealth
+function calculateWealth(){
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0)
+    const wealthEl = document.createElement('div')
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`
+    main.appendChild(wealthEl)
+    
+}
+// update dom
+function updateDOM(providedData = data){
+    // clear main div
+    main.innerHTML = `<h2><strong>Person</strong> Wealth</h2>`
+        providedData.forEach(item => {
+            const element = document.createElement('div')
+            element.classList.add('person')
+            element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`
+            main.appendChild(element)
+        })
+}
+
+// format number as money
+function formatMoney(number){
+    return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');  // 12,345.67
+}
+
+// event listener
+addUserBtn.addEventListener('click', getRandomUser)
+doubleBtn.addEventListener('click', doubleMoney)
+showMillionareBtn.addEventListener('click', showMillionaires)
+sortBtn.addEventListener('click', sortByRichest)
+calculateWealthBtn.addEventListener('click', calculateWealth)
